dfd8bcf5bfcb26b9ff029c8df5c84f4c625b2091
AlejandroMaldonado89 <i.maldonado@uniandes.edu.co>
2025-09-17 08:14:48 -0500

    Punto 1 parte 2

--- src/main/java/co/edu/uniandes/dse/parcial1/entities/MercanciaEntity.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/entities/MercanciaEntity.java
@@ -3,7 +3,9 @@ package co.edu.uniandes.dse.parcial1.entities;
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
@@ -13,6 +15,9 @@ public class MercanciaEntity extends BaseEntity{
     private int codigo_barras;
     private LocalDate fecha_recepcion;
     private int cantidad_disponible;
-    
+
+    @PodamExclude
+    @ManyToOne
+    private UbicacionBodegaEntity ubicacionBodega;
 
 }
--- src/main/java/co/edu/uniandes/dse/parcial1/entities/UbicacionBodegaEntity.java
+++ src/main/java/co/edu/uniandes/dse/parcial1/entities/UbicacionBodegaEntity.java
@@ -1,7 +1,12 @@
 package co.edu.uniandes.dse.parcial1.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
@@ -10,4 +15,8 @@ public class UbicacionBodegaEntity extends BaseEntity{
     private int numero_estante;
     private String canasta_deposita;
     private float peso_maximo;
+
+    @PodamExclude
+    @OneToMany
+    private List<MercanciaEntity> mercancia = new ArrayList();
 }
